73c9ec9ac62151d5c656b4789adecbaa9c67bac2
Xiuheng <wuxhme@gmail.com>
2021-06-05 14:39:44 +0800

    Update quick script

--- quick-rts.sh
+++ quick-rts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 docker-compose -f docker-compose.yml build rts-driver
 docker-compose up rts-driver
-cd /var/lib/docker/volumes/diffbase_datavol/_data || exit
-echo "check ./run_grok/grok_results/Lang-28.affected for selected test classes"
+DATAVOL_LOC=/var/lib/docker/volumes/diffbase_datavol/_data
+echo "check $DATAVOL_LOC/run_grok/grok_results/Lang-28.affected for selected test classes"
